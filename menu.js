b706c78961798471bc48821b552ad95260b896ce
stephaneduperret <75334937+stephaneduperret@users.noreply.github.com>
2025-08-13 10:04:48 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,19 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbar = `
         <div class="navbar">
             <div class="logo">
-                <img src="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/Images/VOé-removebg-preview.png" alt="Logo BeTomorrow">
+                <img src="./Images/VOé-removebg-preview.png" alt="Logo BeTomorrow">
             </div>
             <ul class="menu">
-		<li><a href="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/Convertir A en kVA.html">Convertir I-S</a></li>
-                <li><a href="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/Calculatrice puissance câble et coût.html">Divers</a></li>
-                <li><a href="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/Calculs solaire.html">Solaire</a></li>
-		<li><a href="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/Calculs solaire test.html">Solaire test</a></li>
-                <li><a href="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/Calculs de dates.html">Date</a></li>
-                <li><a href="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/Chute de tension.html">Chute de tension</a></li>
-                <li><a href="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/Calcul achat tubes.html">Tarifs divers</a></li>
-		<li><a href="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/Calcul Alim Armoire.html">Alim Armoire</a></li>
-                <li><a href="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/Transfo.html">Transformateur</a></li>
-		<li><a href="H:/Distribution/07_COMMUN/STEPHANE/Calculatrice/index.html" style="color:blue;">Accueil</a></li>
+                <li><a href="./Convertir A en kVA.html">Convertir I-S</a></li>
+                <li><a href="./Calculatrice puissance câble et coût.html">Divers</a></li>
+                <li><a href="./Calculs solaire.html">Solaire</a></li>
+                <li><a href="./Calculs solaire test.html">Solaire test</a></li>
+                <li><a href="./Calculs de dates.html">Date</a></li>
+                <li><a href="./Chute de tension.html">Chute de tension</a></li>
+                <li><a href="./Calcul achat tubes.html">Tarifs divers</a></li>
+                <li><a href="./Calcul Alim Armoire.html">Alim Armoire</a></li>
+                <li><a href="./Transfo.html">Transformateur</a></li>
+                <li><a href="./index.html" style="color:blue;">Accueil</a></li>
             </ul>
         </div>
     `;
