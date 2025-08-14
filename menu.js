7ce246a16f4df3a1eab6f6ae4adafa7036331757
stephaneduperret <75334937+stephaneduperret@users.noreply.github.com>
2025-08-14 11:37:47 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -19,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <li><a href="./Chute de tension.html">Chute de tension</a></li>
         <li><a href="./Calcul achat tubes.html">Tarifs divers</a></li>
         <li><a href="./Calcul Alim Armoire.html">Alim Armoire</a></li>
+         <li><a href="./calculreseau.html">Calculs Réseau</a></li>
         <li><a href="./Transfo.html">Transformateur</a></li>
         <li><a href="./index.html" style="color:blue;">Accueil</a></li>
       </ul>
@@ -85,4 +86,5 @@ document.addEventListener('DOMContentLoaded', () => {
     lastScrollY = current;
   }, { passive: true });
 });
+
 
